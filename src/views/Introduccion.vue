71c90be19baa570f9576bbfef6f01528ac1da950
lizmanchego <karinamanchego0416@gmail.com>
2025-07-29 14:43:59 -0500

    [ADD] Video Introduccion

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -19,8 +19,8 @@
           .col-lg-12
             figure
               .video
-                iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-            figcaption Video. 
+                iframe(width="560" height="315" src="https://www.youtube.com/embed/nfoLPdlizJI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            figcaption Video. Introducción
 
 </template>
 
